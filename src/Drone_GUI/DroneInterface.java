2a9efeb1df6ed8abd119c4c26d5ad354b514ce5f
Shavin Croos <at015244@student.reading.ac.uk>
2020-12-05 19:26:00 +0000

    Opens empty window, with the options to view about, help and exit the program.

--- src/Drone_GUI/DroneInterface.java
+++ src/Drone_GUI/DroneInterface.java
@@ -1,4 +1,109 @@
 package Drone_GUI;
 
-public class DroneInterface {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.animation.AnimationTimer;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.input.MouseEvent;
+
+public class DroneInterface extends Application{
+    private int CanvasWidth = 400, CanvasHeight = 500;  // Sets the size of the canvas
+    private MyCanvas mc;                                // Calls upon the canvas where system is drawn
+
+    private void showMessage(String TStr, String CStr) { // Shows any message in the form of the title, then the message
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle(TStr);
+        alert.setHeaderText(null);
+        alert.setContentText(CStr);
+        alert.showAndWait();
+    }
+
+    // Shows About box, with information about the simulator
+    private void viewAbout() {
+        showMessage("About", "Drone Simulator 2020 (GUI version)." + "\n" +
+                "Based of the console version and developed by Shavin Croos." + "\n" +
+                "The program shows the movement of randomly placed drones in a given arena" +
+                " in the form of an animation. ");
+    }
+
+    // Shows Help box, with information on how to use the simulator
+    private void viewHelp() {
+        showMessage( "Help", "Click 'Insert Drone' to randomly add a drone to your arena. " + "\n" +
+                "Press 'START' to watch the drones move about in the arena and then press 'STOP' when you want" +
+                " to stop the animation.");
+    }
+
+    MenuBar setMenu() {
+        MenuBar menuBar = new MenuBar();		// create menu
+
+        Menu mInfo = new Menu("Info");			// have entry for help
+        // then add sub menus for About and Help
+        // add the item and then the action to perform
+        MenuItem mAbout = new MenuItem("About");
+        mAbout.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                viewAbout();				// show the about message
+            }
+        });
+        MenuItem mHelp = new MenuItem("Help");
+        mHelp.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                viewHelp();				// show the help message
+            }
+        });
+        mInfo.getItems().addAll(mAbout, mHelp); 	// add submenus to Help
+
+        // now add File menu, which here only has Exit
+        Menu mFile = new Menu("File");
+        MenuItem mExit = new MenuItem("Exit");
+        mExit.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent t) {
+                System.exit(0);						// quit program
+            }
+        });
+        mFile.getItems().addAll(mExit);
+
+        menuBar.getMenus().addAll(mFile, mInfo);	// menu has File and Help
+
+        return menuBar;					// return the menu, so can be added
+    }
+
+    public void start (Stage stagePrimary) throws Exception {
+        stagePrimary.setTitle("Drone Simulator 2020 (GUI version)");
+
+        BorderPane bp = new BorderPane();
+
+        bp.setTop(setMenu());
+
+        Group root = new Group();
+        Canvas canvas = new Canvas(CanvasWidth, CanvasHeight);
+
+        Scene scene = new Scene(bp, CanvasWidth * 1.5, CanvasHeight * 1.2);
+
+        stagePrimary.setScene(scene);
+        stagePrimary.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
 }
--- src/Drone_GUI/MyCanvas.java
+++ src/Drone_GUI/MyCanvas.java
@@ -1,4 +1,135 @@
 package Drone_GUI;
 
-public class MyCanvas {
+import javafx.application.Application;
+import javafx.geometry.VPos;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+
+public class MyCanvas extends Application {
+    int xCanvasSize = 512;				// constants for relevant sizes, default values set
+    int yCanvasSize = 512;
+    GraphicsContext gc;
+
+    /**
+     * onstructor sets up relevant Graphics context and size of canvas
+     * @param g
+     * @param cs
+     */
+    public MyCanvas(GraphicsContext g, int xcs, int ycs) {
+        gc = g;
+        xCanvasSize = xcs;
+        yCanvasSize = ycs;
+    }
+
+    public int getXCanvasSize() {
+        return xCanvasSize;
+    }
+    public int getYCanvasSize() {
+        return yCanvasSize;
+    }
+    /**
+     * clear the canvas
+     */
+    public void clearCanvas() {
+        gc.clearRect(0,  0,  xCanvasSize,  yCanvasSize);		// clear canvas
+    }
+    /**
+     * drawImage ... draws object defined by given image at position and size
+     * @param i		image
+     * @param x		xposition	in range 0..1
+     * @param y
+     * @param sz	size
+     */
+    public void drawImage (Image i, double x, double y, double sz) {
+        // to draw centred at x,y, give top left position and x,y size
+        // sizes/position in range 0.. canvassize
+        gc.drawImage(i, x - sz/2, y - sz/2, sz, sz);
+    }
+
+
+    /**
+     * function to convert char c to actual colour used
+     * @param c
+     * @return Color
+     */
+    Color colFromChar (char c){
+        Color ans = Color.BLACK;
+        switch (c) {
+            case 'y' :	ans = Color.YELLOW;
+                break;
+            case 'w' :	ans = Color.WHITE;
+                break;
+            case 'r' :	ans = Color.RED;
+                break;
+            case 'g' :	ans = Color.GREEN;
+                break;
+            case 'b' :	ans = Color.BLUE;
+                break;
+            case 'o' :	ans = Color.ORANGE;
+                break;
+        }
+        return ans;
+    }
+
+    /**
+     * set the fill colour to color c
+     * @param c
+     */
+    public void setFillColour (Color c) {
+        gc.setFill(c);
+    }
+    /**
+     * show the ball at position x,y , radius r in colour defined by col
+     * @param x
+     * @param y
+     * @param rad
+     * @param col
+     */
+    public void showCircle(double x, double y, double rad, char col) {
+        setFillColour(colFromChar(col));			// set the fill colour
+        showCircle(x, y, rad);						// show the circle
+    }
+
+    /**
+     * show the ball in the current colour at x,y size rad
+     * @param x
+     * @param y
+     * @param rad
+     */
+    public void showCircle(double x, double y, double rad) {
+        gc.fillArc(x-rad, y-rad, rad*2, rad*2, 0, 360, ArcType.ROUND);	// fill circle
+    }
+
+    /**
+     * Show Text .. by writing string s at position x,y
+     * @param x
+     * @param y
+     * @param s
+     */
+    public void showText (double x, double y, String s) {
+        gc.setTextAlign(TextAlignment.CENTER);							// set horizontal alignment
+        gc.setTextBaseline(VPos.CENTER);								// vertical
+        gc.setFill(Color.WHITE);										// colour in white
+        gc.fillText(s, x, y);											// print score as text
+    }
+
+    /**
+     * Show Int .. by writing int i at position x,y
+     * @param x
+     * @param y
+     * @param i
+     */
+    public void showInt (double x, double y, int i) {
+        showText (x, y, Integer.toString(i));
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO Auto-generated method stub
+
+    }
 }
